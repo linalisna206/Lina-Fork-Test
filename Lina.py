3a2142b837be2af033af1167dcf0432b1db9d5a9
linalisna206 <linalisna206@gmail.com>
2025-11-05 12:00:05 +0700

    Create Lina.py
    saya menambahkan program baru

---
+++ Lina.py
@@ -0,0 +1,10 @@
+#Halo ini pertamakali pake github, smoga bisa 
+
+print("Program Pembaca Pikiran")
+angka = int(input("masukkan bilangan yang anda pikirkan : "))
+print()
+print()
+print(f"bilangan yang kamu pikirkan adalah : {angka}")
+
+
+            
